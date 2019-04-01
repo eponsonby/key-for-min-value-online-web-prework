f783a53144284a7653b544688ef2a0b0d50037ca
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-01 15:49:23 +0000

    Automatically backed up by Learn

---
+++ key_for_min.rb
@@ -0,0 +1,22 @@
+hash = {
+  "Nick" => 20,
+  "Erin" => 30,
+  "Kristin" => 60
+}
+
+
+def key_for_min_value(hash)
+  lowest_number = nil
+  key = nil
+  hash.each do |name, number|
+    if lowest_number == nil
+      lowest_number = hash[name]
+      key = name
+    elsif lowest_number > hash[name]
+      lowest_number = hash[name]
+      key = name
+    end
+  end
+  key
+  #return name with lowest number
+end
